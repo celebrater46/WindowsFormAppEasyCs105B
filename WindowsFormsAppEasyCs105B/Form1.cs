cfb337b394d3b7a35652ff998fdd4884fe1a9dee
hideru <jota.16.stigma.world@gmail.com>
2021-11-29 16:22:29 +0900

    test succeeded

--- WindowsFormsAppEasyCs105B/Form1.cs
+++ WindowsFormsAppEasyCs105B/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,45 @@ namespace WindowsFormsAppEasyCs105B
 {
     public partial class Form1 : Form
     {
+        private ChildForm[] cf;
         public Form1()
         {
             InitializeComponent();
+            this.Text = "Test that opens Xml on a child form";
+            this.Width = 400;
+            this.Height = 400;
+            this.IsMdiContainer = true;
+
+            string dir = "C:\\Users\\Enin\\RiderProjects\\WindowsFormsAppEasyCs105B\\WindowsFormsAppEasyCs105B\\";
+            string[] fls = Directory.GetFiles(dir, "*.xml");
+            cf = new ChildForm[fls.Length];
+
+            for (int i = 0; i < fls.Length; i++)
+            {
+                cf[i] = new ChildForm(fls[i]);
+                cf[i].MdiParent = this;
+                cf[i].Show();
+            }
+        }
+    }
+
+    class ChildForm : Form
+    {
+        private TextBox tb;
+
+        public ChildForm(string name)
+        {
+            this.Text = name;
+            tb = new TextBox();
+            tb.Multiline = true;
+            tb.Width = this.Width;
+            tb.Height = this.Height;
+
+            StreamReader sr = new StreamReader(name, System.Text.Encoding.Default);
+            tb.Text = sr.ReadToEnd();
+            sr.Close();
+
+            tb.Parent = this;
         }
     }
 }
